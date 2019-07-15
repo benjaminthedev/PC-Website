13cea66706a750d4458f1cd0d95519efa0973a3d
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2019-07-15 17:09:31 +0100

    Start of project

---
+++ assets/js/scripts.js
@@ -0,0 +1,70 @@
+jQuery(document).ready(function($){
+
+	$('.mobile-menu').on('click',function(){
+		$('.humbergur-menu').addClass('open');
+	});
+	
+	$('.menu-close').on('click',function(){
+		$('.humbergur-menu').removeClass('open');
+	});
+
+
+    $(".testimonial-carousel").owlCarousel({
+        items:3,
+        nav: false,
+        dots: true,
+        autoplay: false,
+        loop: true,
+        margin:60,	        
+        responsiveClass:true,
+	        responsive:{
+	            0:{
+	                items:1,
+	            },
+	            480:{
+	                items:1,
+	            },
+	            768:{
+	                items:2,
+	                margin:30,
+	            },
+	            992:{
+	                items:2,        
+	                margin:40,	
+	            },
+	            1200:{
+	            	items:3
+	            }
+	        }
+
+	});
+
+	$('p > img').unwrap();
+	$('p > a').unwrap();
+
+
+    $("#form-btn,#form-btn-two,#form-btn-three,#form-btn-four").on('click',function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#colophon").offset().top -90
+        }, 1000);
+    });
+
+
+});    
+
+
+
+
+	window.onscroll = function() {myFunction()};
+
+	var header = document.getElementById("myHeader");
+	var sticky = header.offsetTop;
+
+	function myFunction() {
+	if (window.pageYOffset > sticky) {
+		header.classList.add("sticky");
+	} else {
+		header.classList.remove("sticky");
+	}
+	}
