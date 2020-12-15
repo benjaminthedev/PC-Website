23e59cf69587fd6c02ba7a26bc2dbfa6dedb5a22
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-12-15 20:28:13 +0000

    Updated the and files

--- contactus.php
+++ contactus.php
@@ -3,19 +3,13 @@
 
 get_header(); while(have_posts()): the_post(); ?>
 
-<!-- Our Company Section -->
-<div class="container">
-<?php
-$image = get_field('background');
-if( !empty($image) ): ?>
-	<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class="header-images" />
-<?php endif; ?>
-</div>
+
 
 <section class="pages py-7">
 	<div class="container">
 		<div class="row">
 			<div class="col-12">
+				<h1 class="text-center mv7"><?php the_title(); ?></h1>
 				<?php the_content(); ?>
 			</div>
 		</div>
