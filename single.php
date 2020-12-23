5974608cf0c3b6cf8df59989e7f6d7eac51bfcf2
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-12-23 13:54:23 +0000

    updated theme

--- single.php
+++ single.php
@@ -30,7 +30,12 @@ if( !empty($image) ): ?>
 	<div class="container">
 		<div class="row">
 			<div class="col-12">
-				<?php the_content(); ?>
+				<?php the_content(); ?>	<br />
+				
+				<br />
+				<h3 class="text-center">Share this article!</h3>
+				<?php echo do_shortcode( '[TheChamp-Sharing]' ); ?>
+				<?php echo do_shortcode( '[related_post	]' ); ?>
 			</div>
 		</div>
 	</div>
